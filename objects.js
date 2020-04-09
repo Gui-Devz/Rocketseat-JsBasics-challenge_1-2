935885928ba233117a10be94858923dfd0442225
Guilherme <guilherme_devz@hotmail.com>
2020-04-09 19:36:46 -0300

    Implemting objects and arrays

---
+++ objects.js
@@ -0,0 +1,15 @@
+//Criando objetos
+
+const empresa = {
+    nome: "Rocketseat",
+    cor: "Roxo",
+    endereço: {
+        rua: "Rua Guilherme Gembala",
+        numero: 260
+    }
+}
+
+//console.log('A empresa '+ empresa.nome +' está localizada em '+ 
+  //          empresa.endereço.rua +', '+ empresa.endereço.numero);
+
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereço.rua}, ${empresa.endereço.numero}`);
